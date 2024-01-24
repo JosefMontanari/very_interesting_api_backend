7582aded459057d2fa728c46e9a2f03e37480543
JosefMontanari <josef.montanari94@gmail.com>
2024-01-24 15:21:08 +0100

    add controller and model

---
+++ VeryInterestingApiBackend/Controllers/DrinkController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VeryInterestingApiBackend.Models;
+
+namespace VeryInterestingApiBackend.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class DrinkController : ControllerBase
+    {
+        public static List<Drink> Drinnks { get; set; } = new List<Drink>()
+        {
+            new Drink
+            {
+                Id = 1,
+                Name = "Margarita",
+                Ingredients = {"Tequila", "Triple Sec", "Lime Juice"}
+            }
+        };
+    }
+}
---
+++ VeryInterestingApiBackend/Models/Drink.cs
@@ -0,0 +1,10 @@
+﻿namespace VeryInterestingApiBackend.Models
+{
+    public class Drink
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public List<string> Ingredients { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
